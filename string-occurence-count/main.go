3750c64304c39a1001e973a487bf9a876ca5253a
ASinha24 <alka.sinha2404@gmail.com>
2023-10-13 23:13:11 +0530

    added comment

--- string-occurence-count/main.go
+++ string-occurence-count/main.go
@@ -20,16 +20,18 @@ func main() {
 
 func stringOccurrence(str string) {
 	resultMap := make(map[string]int)
+	//iterate through string and save char in map with correspoding count
 	for _, char := range str {
-		resultMap[string(char)] = resultMap[string(char)] + 1
+		resultMap[string(char)]++
 	}
 
+	// add the key of map in string strice
 	key := []string{}
-
 	for k, _ := range resultMap {
 		key = append(key, k)
 	}
 
+	//sort the map on key and value basis key(ascendiing) value(decending)
 	sort.SliceStable(key, func(i, j int) bool {
 		if resultMap[key[i]] == resultMap[key[j]] {
 			return key[i] < key[j]
@@ -37,6 +39,7 @@ func stringOccurrence(str string) {
 		return resultMap[key[i]] > resultMap[key[j]]
 	})
 
+	//print the key an value
 	for _, k := range key {
 		fmt.Println(k, resultMap[k])
 	}
